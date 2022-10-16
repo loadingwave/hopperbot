eab934d0d35a3e2360afe10f90de9c68fd01e0f9
LoadingWave <tda.posthuma@gmail.com>
2022-10-16 14:36:58 +0200

    Finish testing, thread screenshots work now

--- test.py
+++ test.py
@@ -5,12 +5,17 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 from time import sleep
 
+viewport_width = 1500
+viewport_height = 2000
+footer_height = 225
+header_bottom = 53
+
 options = Options()
 options.headless = True
 driver = Chrome(options=options)
 
 driver.set_window_position(0, 0)
-driver.set_window_size(1500, 3000)
+driver.set_window_size(viewport_width, viewport_height)
 
 url = "https://twitter.com/cuptoast/status/1551711157785751553"
 
@@ -19,26 +24,41 @@ driver.get(url)
 
 # Just to make sure all elements load first
 print("sleeping 2 seconds...")
-sleep(2)
+sleep(1.5)
 
+# Scroll to top
 scrolling_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div"
 full_element = driver.find_element(By.XPATH, scrolling_xpath)
 driver.execute_script("arguments[0].scrollIntoView();", full_element)
 
-driver.save_screenshot("fullscreen.png")
-
 print("sleeping 2 seconds...")
-sleep(2)
+sleep(1.5)
 
-# tweet_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[{}]/div/div/div/article"
 tweet_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[{}]"
 
-for i in range(1, 12):
-    print("screenshot ", i)
-    tweet_element = driver.find_element(By.XPATH, tweet_xpath.format(i))
-    tweet_element.screenshot("tweet{}.png".format(i))
+view_bottom = viewport_height - footer_height
 
-    height = tweet_element.rect["height"]
-    ActionChains(driver).scroll_by_amount(0, height).perform()
+for i in range(1, 12):
+    tweet_element = driver.find_element(By.XPATH, tweet_xpath.format(i))
+    tweet_top = tweet_element.rect["y"]
+    tweet_height = tweet_element.rect["height"]
+    tweet_bottom = tweet_top + tweet_height
+    print(
+        "tweet {} bottom is at {} + {} = {}".format(
+            i, tweet_top, tweet_height, tweet_bottom
+        )
+    )
+
+    if tweet_bottom >= view_bottom:
+        to_scroll = tweet_top - header_bottom
+        print("Scrolled by", to_scroll)
+        ActionChains(driver).scroll_by_amount(0, to_scroll).perform()
+        view_bottom += to_scroll
+        header_bottom += to_scroll
+
+        # because we scrolled we now need to relocate the tweet
+        tweet_element = driver.find_element(By.XPATH, tweet_xpath.format(i))
+
+    tweet_element.screenshot("tweet{}.png".format(i))
 
 driver.close()
