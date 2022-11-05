6de3b59fc600b4c8a23d9c8151c2bb8dcc3b6f55
LoadingWave <tda.posthuma@gmail.com>
2022-11-05 19:45:46 +0100

    Add add_person() and get_person()

--- hopperbot/database.py
+++ hopperbot/database.py
@@ -1,6 +1,6 @@
 import logging
 import sqlite3 as sqlite
-from typing import Tuple, Union
+from typing import Tuple, Optional
 from hopperbot.people import Person, adapt_person, convert_person
 
 logger = logging.getLogger("Database")
@@ -9,9 +9,11 @@ logger.setLevel(logging.INFO)
 sqlite.register_adapter(Person, adapt_person)
 sqlite.register_converter("PERSON", convert_person)
 
+FILENAME = "tweets.db"
+
 
 def init_database() -> None:
-    con = sqlite.connect("tweets.db", detect_types=sqlite.PARSE_DECLTYPES)
+    con = sqlite.connect(FILENAME, detect_types=sqlite.PARSE_DECLTYPES)
     cur = con.cursor()
 
     tweets = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='tweets'").fetchone()
@@ -34,8 +36,8 @@ def init_database() -> None:
     con.close()
 
 
-def get_tweet(tweet_id: int) -> Union[None, Tuple[int, int, str]]:
-    tweets_db = sqlite.connect("tweets.db", detect_types=sqlite.PARSE_DECLTYPES)
+def get_tweet(tweet_id: int) -> Optional[Tuple[int, int, str]]:
+    tweets_db = sqlite.connect(FILENAME, detect_types=sqlite.PARSE_DECLTYPES)
 
     result = None
 
@@ -49,7 +51,7 @@ def get_tweet(tweet_id: int) -> Union[None, Tuple[int, int, str]]:
 
 
 def add_tweet(tweet_id: int, tweet_index: int, tumblr_id: int, blogname: str) -> None:
-    tweets_db = sqlite.connect("tweets.db", detect_types=sqlite.PARSE_DECLTYPES)
+    tweets_db = sqlite.connect(FILENAME, detect_types=sqlite.PARSE_DECLTYPES)
 
     with tweets_db:
         tweets_db.execute(
@@ -59,3 +61,30 @@ def add_tweet(tweet_id: int, tweet_index: int, tumblr_id: int, blogname: str) ->
         logger.debug(f"Inserted tweet id {tweet_id} with tumblr id: {tumblr_id}")
 
     tweets_db.close()
+
+
+def add_person(twitter_id: int, person: Person) -> None:
+    database = sqlite.connect(FILENAME, detect_types=sqlite.PARSE_DECLTYPES)
+
+    with database:
+        database.execute(
+            "INSERT INTO twitter_names(twitter_id, person) VALUES(?, ?)",
+            (twitter_id, person),
+        )
+        logger.debug(f"Inserted person {person.name} with twitter id: {twitter_id} into twitter names table")
+
+    database.close()
+
+
+def get_person(twitter_id: int) -> Optional[Person]:
+    database = sqlite.connect(FILENAME, detect_types=sqlite.PARSE_DECLTYPES)
+
+    result = None
+
+    with database:
+        cur = database.execute("SELECT person FROM twitter_names WHERE tweet_id = ?", [twitter_id])
+        result = cur.fetchone()
+
+    database.close()
+
+    return result
