81b50097e56b1ca3363d1da7083596137aaada17
LoadingWave <tda.posthuma@gmail.com>
2022-11-06 09:48:57 +0100

    Remove twitter_data

--- hopperbot/config.py
+++ hopperbot/config.py
@@ -1,5 +1,3 @@
-from hopperbot.people import HE, IT, NONE, SHE, STAR, THEY, XEY, Person
-
 CHANGED = False
 
 twitter_updatables = {
@@ -9,38 +7,4 @@ twitter_updatables = {
     "tommyinnit": "hopperbot-test2",
     "tommyaltinnit": "hopperbot-test2",
     "tomsimons": "hopperbot-test2"
-}
-
-twitter_data = {
-    1222961028926824450: Person("Ranboo", [HE, THEY]),
-    1344189615134003201: Person("Ranboo", [HE, THEY]),
-    1523474396547100672: Person("Ranboo", [HE, THEY]),
-    1032978119370723329: Person("Tommy", [HE]),
-    1373748437309653004: Person("Tommy", [HE]),
-    1421863028866310150: Person("Generation Loss", [NONE]),
-    1067355715004702720: Person("Aimsey", [HE, SHE, STAR, THEY, XEY]),
-    1342232185445744640: Person("Aimsey", [HE, SHE, STAR, THEY, XEY]),
-    1048468752960876544: Person("Crumb", [HE, IT, SHE, THEY]),
-    1334570033754693633: Person("Crumb", [HE, IT, SHE, THEY]),
-    996365064667123713: Person("Tubbo", [HE]),
-    1386661611260284929: Person("Tubbo", [HE]),
-    3182089458: Person("Misfits", [NONE]),
-    2413696970: Person("Technodad", [HE]),
-    2174576629: Person("Wilbur", [HE]),
-    1322952059604393988: Person("Lovejoy", [NONE]),
-    867218617: Person("Charlie Slimecicle", [HE]),
-    1262819056303947784: Person("Charlie Slimecicle", [HE]),
-    856100665193639943: Person("Niki", [SHE]),
-    1341182680893972481: Person("Niki", [SHE]),
-    1554954456831336448: Person("Niki", [SHE]),
-    517077573: Person("Markiplier", [HE]),
-    127954853: Person("Philza", [HE]),
-    921818190769983489: Person("Purpled", [HE]),
-    1035136817669058560: Person("Billzo", [HE]),
-    1494081471207849985: Person("Billzo", [HE]),
-    1171833930095517699: Person("MCC", [NONE]),
-    1554195344: Person("Scott Smajor", [HE]),
-    538659963: Person("Sneegsnag", [HE]),
-    731186310552158208: Person("Jack Manifold", [HE]),
-    1337155933735186432: Person("Jack Manifold", [HE]),
 }
