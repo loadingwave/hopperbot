cc54472470d8bef925910332ea0ff43b7697f5b2
LoadingWave <tda.posthuma@gmail.com>
2022-11-12 20:54:39 +0100

    Add print_post

--- hopperbot/youtube.py
+++ hopperbot/youtube.py
@@ -1,4 +1,5 @@
 from aiohttp import web
+from pprint import pprint
 
 
 async def handle(request: web.Request):
@@ -6,9 +7,15 @@ async def handle(request: web.Request):
     text = "Hello, " + name
     return web.Response(text=text)
 
+
+async def print_post(request: web.Request):
+    pprint(request.post())
+    return web.Response(status=204)
+
 app = web.Application()
 app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+                web.get('/{name}', handle),
+                web.post('/', print_post)])
 
 if __name__ == '__main__':
     web.run_app(app)
